3ea2176feb3a7286cbb9a3578b06266d56c7d535
zostale <shyamalanadkat@gmail.com>
2017-09-12 16:27:23 -0500

    closing-proj

--- s0/wl.cpp
+++ s0/wl.cpp
@@ -1,244 +1,243 @@
-//  File: wl.cpp
-//  Description: Main
-//  Student Name: Shyamal Anadkat
-//  UW Campus ID: 9071804893
-//  email: anadkat@wisc.edu
-
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <algorithm>
-#include <sstream>
-#include <ctype.h>
-#include <stdlib.h>
-#include <iterator>
-#include <vector>
+/** 
+* @file wl.cpp
+* @description Main(wl.cpp)
+* @author Shyamal Anadkat
+* @email anadkat@wisc.edu
+* @StudentID 9071804893
+*/
 #include "wl.h"
 
-using namespace std;
 BSTree* tree = new BSTree;
 
 int main() {
-    tree->root = NULL;
-    bool end = false;
+	tree->root = NULL;
+	bool end = false;
 
-    string cmd;
+	string cmd;
 
-    while (!end) {
+	while (!end) {
         cout << ">";        // prompt
         cin.clear();
         getline(cin, cmd);  // get input into cmd
         cin.clear();
 
         // split by whitespace and copy to vector
-        transform(cmd.begin(), cmd.end(), cmd.begin(), ::tolower);
         istringstream iss(cmd);
         vector<string> tokens;
         copy(istream_iterator<string>(iss), istream_iterator<string>(),
-             back_inserter(tokens));
-
-        // print vector
-        // for (vector<string>::iterator it = tokens.begin(); it != tokens.end();
-        // ++it) {
-        // cout << *it;
+        	back_inserter(tokens));
         parseInput(tokens);
     }
     return 0;
 }
 
 bool parseInput(vector<string> tokens) {
-    if (tokens.size() < 2) {
-        if (tokens.at(0) == "end") {
-            tree->deleteTree(tree->root);
-            exit(0);
-        } else if (tokens.at(0) == "new") {
-            tree->deleteTree(tree->root);
-        }
 
-        else {
-            invalidCmd();
-        }
-    }
+	string token0;
+	if(tokens.size() > 0) {
+		token0 = toLower(tokens.at(0));
+	}
 
-    else if (tokens.at(0) == "load" && tokens.size() == 2) {
-        if(loadFileIntoBST(tokens.at(1)) == -1) {
-            invalidCmd();
-        }
-        //print_tree(tree->root);
-    }
 
-    else if (tokens.at(0) == "locate" && tokens.size() == 3) {
+	if (tokens.size() < 2) {	
+		if (token0 == "end") {
+		 	if(tree->root != NULL) {
+				tree->deleteTree(tree->root);
+			}
+			
+			if(tree != NULL) {
+				delete tree;
+			}
 
-        string word = tokens.at(1);
-        string num = tokens.at(2);
+			exit(0);
+		} else if (token0 == "new") {
+			if(tree->root != NULL) {
+				tree->deleteTree(tree->root);
+			}
+			if(tree != NULL) {
+				delete tree;
+			}
+			tree = new BSTree();
+		}
 
+		else {
+			invalidCmd();
+		}
+	}
+
+	else if (token0 == "load" && tokens.size() == 2) {
+		if(loadFileIntoBST(tokens.at(1)) == -1) {
+			invalidCmd();
+		}
+	}
+
+	else if (token0 == "locate" && tokens.size() == 3) {
+
+		string word = toLower(tokens.at(1));
+		string num = toLower(tokens.at(2));
 
         // check for invalid inputs
-        for (unsigned int i = 0; i < word.size(); i++) {
-            char tmp = word[i];
-            if (!(isalnum(tmp) or tmp == 39)) {
-                invalidCmd();
-                return false;
-            }
-        }
+		for (unsigned int i = 0; i < word.size(); i++) {
+			char tmp = word[i];
+			if (!(isalnum(tmp) or tmp == 39)) {
+				invalidCmd();
+				return false;
+			}
+		}
 
-        bool isValidNum = (num.find_first_not_of( "0123456789" ) == string::npos);
-        if(!isValidNum) {
-            invalidCmd();
-            return false;
-        }
+		bool isValidNum = 
+		(num.find_first_not_of( "0123456789" ) == string::npos);
+		if(!isValidNum) {
+			invalidCmd();
+			return false;
+		}
 
         //locate now
-        int n = atoi(num.c_str());
-        int retVal = locate(tree->root, word, n, 1);
+		int n = atoi(num.c_str());
+		int retVal = locate(tree->root, word, n, 1);
 
-        if( retVal == -1) {
-            cout << "No matching entry\n";
-        } else {
-            cout << retVal << "\n";
-        }
-    }
+		if( retVal == -1) {
+			cout << "No matching entry\n";
+		} else {
+			cout << retVal << "\n";
+		}
+	}
 
-    else {
-        invalidCmd();
-    }
+	else {
+		invalidCmd();
+	}
 
-    return true;
+	return true;
 }
 
 // prints invalid command to output stream
 void invalidCmd() {
-    cout << "ERROR: Invalid command";
-    cout << '\n';
+	cout << "ERROR: Invalid command";
+	cout << '\n';
 }
-
 
 // curr : current occurence, n: given occurrence
 int locate(BST_Node* root, string key, int n, int curr) {
 
     // fail and return -1
-    if (root == NULL) {
-        //cout << "null";
-        return -1;
-    }
+	if (root == NULL) {
+		return -1;
+	}
 
-    if (key == root->word) {
-        ///cout << "equals";
+	if (key == root->word) {
 
-        if (n == curr) {
-            //printf("n: %d", n);
-            //printf("curr: %d", curr);
-            return root->count;
-        } else {
-            curr++;
-            return locate(root->right, key, n, curr);
-        }
-    }
+		if (n == curr) {
+			return root->count;
+		} else {
+			curr++;
+			return locate(root->right, key, n, curr);
+		}
+	}
 
-    else if (key < root->word) {
-        return locate(root->left, key, n, curr);
-    }
+	else if (key < root->word) {
+		return locate(root->left, key, n, curr);
+	}
 
-    else if (key > root->word) {
-        return locate(root->right, key, n, curr);
-    }
+	else if (key > root->word) {
+		return locate(root->right, key, n, curr);
+	}
 }
 
 
 void insert(BST_Node* root, BST_Node* newnode) {
 
-    if (root->word > newnode->word) {
-        if (root->left != NULL) {
-            insert(root->left, newnode);
-        } else {
-            root->left = newnode;
-            return;
-        }
-    }
+	if (root->word > newnode->word) {
+		if (root->left != NULL) {
+			insert(root->left, newnode);
+		} else {
+			root->left = newnode;
+			return;
+		}
+	}
 
-    else {
-        if (root->right != NULL) {
-            insert(root->right, newnode);
-        } else {
-            root->right = newnode;
-            return;
-        }
-    }
+	else {
+		if (root->right != NULL) {
+			insert(root->right, newnode);
+		} else {
+			root->right = newnode;
+			return;
+		}
+	}
 }
 
 
 
 int loadFileIntoBST(string fileName) {
-    int wordCount = 1;
+	int wordCount = 1;
+
     // load file with relative or absolute path
-    ifstream infile(fileName.c_str());
-    string stream;
-    string key = "";
+	ifstream infile(fileName.c_str());
+	string stream;
+	string key = "";
 
-    if (infile.is_open()) {
-        while (getline(infile, stream)) {
+	if (infile.is_open()) {
+		while (getline(infile, stream)) {
             // cout << stream;
-            for (unsigned int i = 0; i < stream.size(); i++) {
-                char tmp = stream[i];
+			for (unsigned int i = 0; i < stream.size(); i++) {
+				char tmp = stream[i];
                 // cout << tmp;
-                if (isalnum(tmp) or tmp == 39) {
-                    key += tmp;
-                } else {
-                    if (key.size() > 0) {
-                        transform(key.begin(), key.end(), key.begin(), ::tolower);
-                        BST_Node* newnode = new BST_Node(key, wordCount);
+				if (isalnum(tmp) or tmp == 39) {
+					key += tmp;
+				} else {
+					if (key.size() > 0) {
+						transform(key.begin(), key.end(), key.begin(), ::tolower);
+						BST_Node* newnode = new BST_Node(key, wordCount);
 
-                        if (tree->root == NULL) {
-                            tree->root = newnode;
-                        } else {
-                            insert(tree->root, newnode);
-                        }
-                        key = "";
-                        wordCount++;
-                    }
-                }
-            }
+						if (tree->root == NULL) {
+							tree->root = newnode;
+						} else {
+							insert(tree->root, newnode);
+						}
+						key = "";
+						wordCount++;
+					}
+				}
+			}
 
-            if (key.size() > 0) {
-                transform(key.begin(), key.end(), key.begin(), ::tolower);
-                BST_Node* newnode = new BST_Node(key, wordCount);
+			if (key.size() > 0) {
+				transform(key.begin(), key.end(), key.begin(), ::tolower);
+				BST_Node* newnode = new BST_Node(key, wordCount);
 
-                if (tree->root == NULL) {
-                    tree->root = newnode;
-                } else {
-                    insert(tree->root, newnode);
-                }
-                key = "";
-                wordCount++;
-            }
-        }
-        infile.close();
-    } else {
-        return -1;
-    }
+				if (tree->root == NULL) {
+					tree->root = newnode;
+				} else {
+					insert(tree->root, newnode);
+				}
+				key = "";
+				wordCount++;
+			}
+		}
+		infile.close();
+	} else {
+		return -1;
+	}
 
-    return wordCount;
+	return wordCount;
 }
 
 
 void BSTree::deleteTree(BST_Node* root) {
 
-    if(root == NULL) return;
+	if(root == NULL) return;
 
-    if(root != NULL) {
-    deleteTree(root->left);
-    deleteTree(root->right);
-    
-    delete root;
-  }
+	if(root != NULL) {
+		deleteTree(root->left);
+		deleteTree(root->right);
+
+		delete root;
+	}
 }
 
-void print_tree(BST_Node *n) {
-    if (n) {
-        print_tree(n->left);
-        cout << n->word;
-        cout << n->count;
-        cout << "\n";
-        print_tree(n->right);
-    }
+string toLower(string in) {
+	for(unsigned int i = 0; i < in.size(); i++){
+		if(in[i] >= 'A' && in[i] <= 'Z'){
+			in[i] = in[i]-('Z'-'z');
+		}
+	}
+	return in;
 }
--- s0/wl.h
+++ s0/wl.h
@@ -1,25 +1,41 @@
-//  File: wl.h
-//  Description: Header file
-//  Student Name: Shyamal Anadkat
-//  UW Campus ID: 9071804893
-//  email: anadkat@wisc.edu
+/** 
+* @file wl.h
+* @description header file
+* @author Shyamal Anadkat
+* @email anadkat@wisc.edu
+* @StudentID 9071804893
+*/
 
 #ifndef WL_H
 #define WL_H
+
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <ctype.h>
+#include <stdlib.h>
+#include <iterator>
+#include <vector>
+
 using namespace std;
 
-
-// Represents a single binary search tree node
+/** 
+* @brief Represents a single binary search tree node
+*
+*/
 class BST_Node {
 public:
 
-    string word;		// key holding data
-    int count; 			// word numbering
-    BST_Node *left; 	// ptr to left
-    BST_Node *right;	// ptr to right
+    string word;		/*!< key holding data */
+    int count; 			/*!< word numbering   */
+    BST_Node *left; 	/*!< ptr to left tree */
+    BST_Node *right;	/*!< ptr to right tree*/
 
-    BST_Node();			//constructor
-    ~BST_Node() {};		//destructor
+    BST_Node();			/*!< constructor */
+    ~BST_Node() {};		/*!< destructor  */
 
     BST_Node(string in, int cnt) {
         word = in;
@@ -28,7 +44,9 @@ public:
     }
 };
 
-// Represents a single binary search tree
+/** 
+* @brief  Represents a single binary search tree
+*/
 class BSTree {
 
 public:
@@ -40,15 +58,63 @@ public:
 
     ~BSTree() {  }
 
+/**
+* Frees tree's memory
+* @param root root of BST tree
+*
+*/
     void deleteTree(BST_Node* root);
 };
 
+
+/**
+* helper function to transform string lowercase
+* @param in an input string
+* @return lower case output string
+*
+*/
+string toLower(string in);
+
+/**
+* parses given tokens
+* @param tokens vector of input tokens
+* @return true if correctly parsed
+*
+*/
 bool parseInput(vector<string> tokens);
+
+/**
+* prints out invalid command string
+*/
 void invalidCmd();
+
+/**
+* Loads the given file into the search tree
+* @param fileName location of file
+* @return total file wordcount
+* Returns -1 if invalid
+*
+*/
 int  loadFileIntoBST(string fileName);
+/**
+* Inserts a BST_Node into the tree
+* @param root word-search tree root
+* @param newnode the node to be added
+*
+*/
 void insert(BST_Node* root, BST_Node* newnode);
+
+/**
+* Locates the word given input params
+* @param root root of the tree
+* @param key word to be searched
+* @param n nth occurence of the word
+* @param curr ref. to occurence of key so far
+* @return location of the resulting word in file
+*
+*/
 int  locate(BST_Node* root, string key, int n, int curr);
 
-void print_tree(BST_Node* n);
-//end of header guard
+
+// end of header guard
 #endif
